3ef194f98d446dbc572bca1258b93d9a7842fb3b
jeremytondo <jmargari@gmail.com>
2024-11-30 18:42:28 +0000

    Reverted to default install location

--- boot.sh
+++ boot.sh
@@ -16,13 +16,13 @@ sudo apt-get update >/dev/null
 sudo apt-get install -y git >/dev/null
 
 echo "Cloning Omakub Lite..."
-rm -rf ~/.local/share/omakub-lite
-git clone https://github.com/jeremytondo/omakub-lite.git ~/.local/share/omakub-lite >/dev/null
+rm -rf ~/.local/share/omakub
+git clone https://github.com/jeremytondo/omakub-lite.git ~/.local/share/omakub >/dev/null
 if [[ $OMAKUB_REF != "master" ]]; then
-	cd ~/.local/share/omakub-lite
+	cd ~/.local/share/omakub
 	git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
 	cd -
 fi
 
 echo "Installation starting..."
-source ~/.local/share/omakub-lite/install.sh
+source ~/.local/share/omakub/install.sh

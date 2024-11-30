20655b871dfe8164c354f3de7383f345afb1b91d
jeremytondo <jmargari@gmail.com>
2024-11-30 15:38:22 +0000

    Updated to Omakub Lite git refs

--- boot.sh
+++ boot.sh
@@ -1,28 +1,28 @@
 set -e
 
-ascii_art='________                  __        ___.
-\_____  \   _____ _____  |  | ____ _\_ |__
- /   |   \ /     \\__   \ |  |/ /  |  \ __ \
-/    |    \  Y Y  \/ __ \|    <|  |  / \_\ \
-\_______  /__|_|  (____  /__|_ \____/|___  /
-        \/      \/     \/     \/         \/
+ascii_art='________                  __        ___.     .____    .__  __          
+\_____  \   _____ _____  |  | ____ _\_ |__   |    |   |__|/  |_  ____  
+ /   |   \ /     \\__  \ |  |/ /  |  \ __ \  |    |   |  \   __\/ __ \ 
+/    |    \  Y Y  \/ __ \|    <|  |  / \_\ \ |    |___|  ||  | \  ___/ 
+\_______  /__|_|  (____  /__|_ \____/|___  / |_______ \__||__|  \___  >
+        \/      \/     \/     \/         \/          \/             \/ 
 '
 
 echo -e "$ascii_art"
-echo "=> Omakub is for fresh Ubuntu 24.04 installations only!"
+echo "=> Omakub Lite is for fresh Ubuntu 24.04 installations only!"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
 sudo apt-get update >/dev/null
 sudo apt-get install -y git >/dev/null
 
-echo "Cloning Omakub..."
-rm -rf ~/.local/share/omakub
-git clone https://github.com/basecamp/omakub.git ~/.local/share/omakub >/dev/null
+echo "Cloning Omakub Lite..."
+rm -rf ~/.local/share/omakub-lite
+git clone https://github.com/jeremytondo/omakub-lite.git ~/.local/share/omakub-lite >/dev/null
 if [[ $OMAKUB_REF != "master" ]]; then
-	cd ~/.local/share/omakub
+	cd ~/.local/share/omakub-lite
 	git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
 	cd -
 fi
 
 echo "Installation starting..."
-source ~/.local/share/omakub/install.sh
+source ~/.local/share/omakub-lite/install.sh

c3fb9882535b0ae7fa220b745362634a1cdb9a20
jeremytondo <jmargari@gmail.com>
2024-12-01 02:59:30 +0000

    Updated main branch name

--- boot.sh
+++ boot.sh
@@ -18,7 +18,7 @@ sudo apt-get install -y git >/dev/null
 echo "Cloning Omakub Lite..."
 rm -rf ~/.local/share/omakub
 git clone https://github.com/jeremytondo/omakub-lite.git ~/.local/share/omakub >/dev/null
-if [[ $OMAKUB_REF != "master" ]]; then
+if [[ $OMAKUB_REF != "main" ]]; then
 	cd ~/.local/share/omakub
 	git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
 	cd -
